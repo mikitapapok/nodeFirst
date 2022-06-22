2509b6e2e5382266eade6f00a6d08a4b1e270b23
mikita.papok <papokmikita@gmail.com>
2022-06-22 12:23:12 +0300

    express routing and templates

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
+
+const rootDIr = require("../utils/routPath");
 const router = express.Router();
 router.get("/product-card", (req, res) => {
-  res.sendFile(path.join(__dirname, "../", "view", "admin.html"));
+  res.sendFile(path.join(rootDIr, "view", "admin.html"));
 });
 router.post("/product-card", (req, res) => {
   console.log(req.body.aloha);
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
 const path = require("path");
+
+const rootDIr = require("../utils/routPath");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../", "view", "shop.html"));
+  res.sendFile(path.join(rootDIr, "view", "shop.html"));
 });
 
 module.exports = router;
